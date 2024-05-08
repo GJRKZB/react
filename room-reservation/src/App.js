3250b85365c29a2ba22f50137558b579e2b42cf6
GJRKZB <Robinkonijnendijk@gmail.com>
2024-05-08 12:55:15 +0200

    render components, css styling

---
+++ room-reservation/src/App.js
@@ -0,0 +1,12 @@
+import RoomList from "./components/RoomList";
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+
+export default function App() {
+  return (
+    <div className="App">
+      <RoomList />
+      <Sidebar />
+    </div>
+  );
+}
